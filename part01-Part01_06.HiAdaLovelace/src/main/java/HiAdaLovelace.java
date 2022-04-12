e4e9729dc2e02185de815adce6b78e21c8fe2bd3
TanjaTJ <87280959+TanjaTJ@users.noreply.github.com>
2022-04-12 22:12:55 +0300

    Add files via upload

---
+++ part01-Part01_06.HiAdaLovelace/src/main/java/HiAdaLovelace.java
@@ -0,0 +1,10 @@
+import java.net.SocketTimeoutException;
+
+public class HiAdaLovelace {
+
+    public static void main(String[] args) {
+        String name = "Ada Lovelace";
+System.out.println("Hi " + name + "!");
+
+    }
+}
